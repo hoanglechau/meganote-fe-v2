1a0403dd1e5528be8d9d8b0f4242c57d431ed683
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-05-29 11:49:27 +0700

    Create EditUserForm.jsx

---
+++ src/features/users/EditUserForm.jsx
@@ -0,0 +1,197 @@
+import { useState, useEffect } from "react";
+import { useUpdateUserMutation, useDeleteUserMutation } from "./usersApiSlice";
+import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSave, faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import { ROLES } from "../../config/roles";
+
+// Regex patterns to check validity of username and password
+const USER_REGEX = /^[A-z]{3,20}$/;
+const PWD_REGEX = /^[A-z0-9!@#$%]{4,12}$/;
+
+const EditUserForm = ({ user }) => {
+  // Getting the 'updateUser' function and statuses after running this function from the custom hook
+  const [updateUser, { isLoading, isSuccess, isError, error }] =
+    useUpdateUserMutation();
+
+  // Also change the names of the default statuses received from this custom hook after running the 'deleteUser' function
+  const [
+    deleteUser,
+    { isSuccess: isDelSuccess, isError: isDelError, error: delerror },
+  ] = useDeleteUserMutation();
+
+  const navigate = useNavigate();
+
+  const [username, setUsername] = useState(user.username);
+  const [validUsername, setValidUsername] = useState(false);
+  const [password, setPassword] = useState("");
+  const [validPassword, setValidPassword] = useState(false);
+  const [roles, setRoles] = useState(user.roles);
+  const [active, setActive] = useState(user.active);
+
+  // Side effects to check the validity of username and password
+  useEffect(() => {
+    setValidUsername(USER_REGEX.test(username));
+  }, [username]);
+
+  useEffect(() => {
+    setValidPassword(PWD_REGEX.test(password));
+  }, [password]);
+
+  // Side effect to reset the form after a successful update or delete
+  useEffect(() => {
+    if (isSuccess || isDelSuccess) {
+      setUsername("");
+      setPassword("");
+      setRoles([]);
+      navigate("/dash/users");
+    }
+  }, [isSuccess, isDelSuccess, navigate]); // including 'navigate' here to avoid a warning
+
+  // Event handlers
+  const onUsernameChanged = e => setUsername(e.target.value);
+
+  const onPasswordChanged = e => setPassword(e.target.value);
+
+  const onRolesChanged = e => {
+    const values = Array.from(e.target.selectedOptions, option => option.value);
+    setRoles(values);
+  };
+
+  // Set the 'active' state to the opposite of its current value for the checkbox
+  const onActiveChanged = () => setActive(prev => !prev);
+
+  const onSaveUserClicked = async e => {
+    // Password is optional when updating, so we must update the user in two different ways
+    if (password) {
+      // Run the function from the custom hook and passing in the new states as arguments
+      await updateUser({ id: user.id, username, password, roles, active });
+    } else {
+      await updateUser({ id: user.id, username, roles, active });
+    }
+  };
+
+  const onDeleteUserClicked = async () => {
+    // Run the action from the custom hook
+    await deleteUser({ id: user.id });
+  };
+
+  // Options for user roles drop-down
+  const options = Object.values(ROLES).map(role => {
+    return (
+      <option key={role} value={role}>
+        {" "}
+        {role}
+      </option>
+    );
+  });
+
+  let canSave;
+  // Password is optional, so we must check for its validity only if it exists -> check in two different ways
+  if (password) {
+    canSave =
+      [roles.length, validUsername, validPassword].every(Boolean) && !isLoading;
+  } else {
+    canSave = [roles.length, validUsername].every(Boolean) && !isLoading;
+  }
+
+  // Show error messages and highlight incomplete/invalid fields
+  const errClass = isError || isDelError ? "errmsg" : "offscreen";
+  const validUserClass = !validUsername ? "form__input--incomplete" : "";
+  const validPwdClass =
+    password && !validPassword ? "form__input--incomplete" : "";
+  const validRolesClass = !roles.length ? "form__input--incomplete" : "";
+
+  // This is based on whether we are using the update or delete mutation
+  // If error or delete error doesn't exist, then we don't want to display anything
+  const errContent = (error?.data?.message || delerror?.data?.message) ?? "";
+
+  // Content to be rendered
+  const content = (
+    <>
+      <p className={errClass}>{errContent}</p>
+
+      <form className="form" onSubmit={e => e.preventDefault()}>
+        <div className="form__title-row">
+          <h2>Edit User</h2>
+          <div className="form__action-buttons">
+            <button
+              className="icon-button"
+              title="Save"
+              onClick={onSaveUserClicked}
+              disabled={!canSave}
+            >
+              <FontAwesomeIcon icon={faSave} />
+            </button>
+            <button
+              className="icon-button"
+              title="Delete"
+              onClick={onDeleteUserClicked}
+            >
+              <FontAwesomeIcon icon={faTrashCan} />
+            </button>
+          </div>
+        </div>
+        <label className="form__label" htmlFor="username">
+          Username: <span className="nowrap">[3-20 letters]</span>
+        </label>
+        <input
+          className={`form__input ${validUserClass}`}
+          id="username"
+          name="username"
+          type="text"
+          autoComplete="off"
+          value={username}
+          onChange={onUsernameChanged}
+        />
+
+        <label className="form__label" htmlFor="password">
+          Password: <span className="nowrap">[empty = no change]</span>{" "}
+          <span className="nowrap">[4-12 chars incl. !@#$%]</span>
+        </label>
+        <input
+          className={`form__input ${validPwdClass}`}
+          id="password"
+          name="password"
+          type="password"
+          value={password}
+          onChange={onPasswordChanged}
+        />
+
+        <label
+          className="form__label form__checkbox-container"
+          htmlFor="user-active"
+        >
+          ACTIVE:
+          <input
+            className="form__checkbox"
+            id="user-active"
+            name="user-active"
+            type="checkbox"
+            checked={active}
+            onChange={onActiveChanged}
+          />
+        </label>
+
+        <label className="form__label" htmlFor="roles">
+          ASSIGNED ROLES:
+        </label>
+        <select
+          id="roles"
+          name="roles"
+          className={`form__select ${validRolesClass}`}
+          multiple={true}
+          size="3"
+          value={roles}
+          onChange={onRolesChanged}
+        >
+          {options}
+        </select>
+      </form>
+    </>
+  );
+
+  return content;
+};
+
+export default EditUserForm;
