85fa8e8c76244cb17fe31817f7693bdd6053eba9
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-05-29 16:54:23 +0700

    Update Public.jsx

--- src/components/Public.jsx
+++ src/components/Public.jsx
@@ -1,21 +1,30 @@
 import { Link } from "react-router-dom";
+import { Paper, Box, Typography, Button } from "@mui/material";
 
 const Public = () => {
   const content = (
-    <section className="public">
-      <header>
-        <h1>
-          Welcome to <span className="nowrap">Meganote!</span>
-        </h1>
-      </header>
-      <main className="public__main">
-        <p>The ultimate note taking experience!</p>
-      </main>
-      <footer>
-        <Link to="/login">Employee Login</Link>
-      </footer>
-    </section>
+    <Paper component="section" className="public">
+      <Box component="header">
+        <Typography variant="h2">
+          Welcome to{" "}
+          <Box component="span" className="nowrap">
+            Meganote!
+          </Box>
+        </Typography>
+      </Box>
+      <Box component="main" className="public__main">
+        <Typography variant="h5">
+          The ultimate note taking experience!
+        </Typography>
+      </Box>
+      <Box component="footer">
+        <Button variant="outlined">
+          <Link to="/login">User Login</Link>
+        </Button>
+      </Box>
+    </Paper>
   );
+
   return content;
 };
 
