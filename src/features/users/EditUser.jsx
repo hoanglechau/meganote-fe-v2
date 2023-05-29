cb8b388a87aca8f720e5169328f145585a33b7b5
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-05-29 11:49:25 +0700

    Create EditUser.jsx

---
+++ src/features/users/EditUser.jsx
@@ -0,0 +1,28 @@
+import { useParams } from "react-router-dom";
+import EditUserForm from "./EditUserForm";
+import { useGetUsersQuery } from "./usersApiSlice";
+import PulseLoader from "react-spinners/PulseLoader";
+import useTitle from "../../hooks/useTitle";
+
+const EditUser = () => {
+  // Custom hook to set the page title
+  useTitle("Meganote: Edit User");
+
+  // Get user id parameter out of the URL
+  const { id } = useParams();
+
+  const { user } = useGetUsersQuery("usersList", {
+    selectFromResult: ({ data }) => ({
+      user: data?.entities[id],
+    }),
+  });
+
+  if (!user) return <PulseLoader color={"#FFF"} />;
+
+  // Content to be rendered
+  const content = <EditUserForm user={user} />;
+
+  return content;
+};
+
+export default EditUser;
