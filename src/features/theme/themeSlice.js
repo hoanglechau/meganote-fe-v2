6adbfd266f4acb06ee5265b1bead96d9aab2ec0a
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-05-29 14:20:00 +0700

    Create themeSlice.js

---
+++ src/features/theme/themeSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  theme: "dark",
+};
+
+const themeSlice = createSlice({
+  name: "theme",
+  initialState,
+  reducers: {
+    toggleTheme(state) {
+      return {
+        theme: state.theme === "light" ? "dark" : "light",
+      };
+    },
+  },
+});
+
+// Export the actions from the reducer
+export const { toggleTheme } = themeSlice.actions;
+
+export default themeSlice.reducer;
