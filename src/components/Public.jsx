709ac15f9b07ed8b4989e46a4e6166a18c46dbcc
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-05-29 11:48:53 +0700

    Create Public.jsx

---
+++ src/components/Public.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+const Public = () => {
+  const content = (
+    <section className="public">
+      <header>
+        <h1>
+          Welcome to <span className="nowrap">Meganote!</span>
+        </h1>
+      </header>
+      <main className="public__main">
+        <p>The ultimate note taking experience!</p>
+      </main>
+      <footer>
+        <Link to="/login">Employee Login</Link>
+      </footer>
+    </section>
+  );
+  return content;
+};
+
+export default Public;
