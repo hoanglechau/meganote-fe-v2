945283230887bdd035f70bff608f066bfc7401ba
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-05-29 11:48:45 +0700

    Create DashFooter.jsx

---
+++ src/components/DashFooter.jsx
@@ -0,0 +1,41 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate, useLocation } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+
+const DashFooter = () => {
+  // Get the username and status from the useAuth custom hook
+  const { username, status } = useAuth();
+
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  // Navigate to the home page when the home button is clicked
+  const onGoHomeClicked = () => navigate("/dash");
+
+  let goHomeButton = null;
+  if (pathname !== "/dash") {
+    goHomeButton = (
+      <button
+        className="dash-footer__button icon-button"
+        title="Home"
+        onClick={onGoHomeClicked}
+      >
+        <FontAwesomeIcon icon={faHouse} />
+      </button>
+    );
+  }
+
+  // Content to be rendered
+  const content = (
+    <footer className="dash-footer">
+      {goHomeButton}
+      <p>Current User: {username}</p>
+      <p>Status: {status}</p>
+    </footer>
+  );
+
+  return content;
+};
+
+export default DashFooter;
