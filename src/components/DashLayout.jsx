133d551bc46b3a3cf703fc2efc2f15bd3b7a5b82
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-05-30 13:50:18 +0700

    Update DashLayout.jsx

--- src/components/DashLayout.jsx
+++ src/components/DashLayout.jsx
@@ -1,15 +1,17 @@
 import { Outlet } from "react-router-dom";
 import DashHeader from "./DashHeader";
 import DashFooter from "./DashFooter";
-import { Paper } from "@mui/material";
+import { Paper, Divider } from "@mui/material";
 
 const DashLayout = () => {
   return (
     <>
       <DashHeader />
+      <Divider />
       <Paper className="dash-container">
         <Outlet />
       </Paper>
+      <Divider />
       <DashFooter />
     </>
   );
